9b18793d3a7ea582efb2683dca6a3bf56d96a6f5
oncekcm <59295554+oncekcm@users.noreply.github.com>
2024-06-19 19:53:59 -0400

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -1,13 +1,24 @@
 import Sequelize from "sequelize";
-
+/*export const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './database.sqlite', // Archivo de la base de datos SQLite
+  logging: false*/
 export const sequelize = new Sequelize(
-  "projectsdb", // db name,
-  "postgres", // username
-  "dragon11", // password
+  "projectsdb_sfhi", // db name,
+  "projectsdb_sfhi_user", // username
+  "qiJ9NG9bPVtIURwUknt5lAeI6liQObNt", // password
   {
-    host: "localhost",
-    dialect: "postgres",
-    logging: true,
+    host: 'dpg-cpec6avsc6pc739dhr2g-a.oregon-postgres.render.com', // Hostname
+    port: 5432, // Port
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true, // Requiere SSL
+        rejectUnauthorized: false // Permite conexiones no autorizadas (útil para desarrollo)
+      }
+    },
+    logging: false, // Opcional: desactiva el registro de SQL en la consola
+  
     // pool: {
     //   max: 5,
     //   min: 0,
